878bf5b1be5d87214b8e89070b2b2bf6d69e70f7
Marioo83GitHub <mcarbajalg@unah.hn>
2025-12-22 09:05:53 -0600

    estilizado ligeramente login

--- resources/views/livewire/user-login.blade.php
+++ resources/views/livewire/user-login.blade.php
@@ -1,9 +1,17 @@
-<div class="flex flex-col">
-    <label for="">Ingrese su Email</label>
-    <input wire:model="email" class="border" type="text">
-    <label for="">Ingrese su Contraseña</label>
-    <input wire:model="password" class="border" type="password">
-    <button wire:click="ingresar" class="border-2 rounded p-1">Ingresar</button>
-    <button wire:click="checkAuth" class="border-2 rounded p-1">Verificar</button>
-    <label for="">{{ $reg }}</label>
+<div class="flex h-screen items-center justify-center">
+
+    <section class="flex flex-col w-72 p-6 rounded-lg shadow-md shadow-gray-300 border border-gray-300">
+
+        <h2 class="text-center mb-4 font-semibold text-lg">Iniciar Sesión</h2>
+
+        <label class="mb-1">Ingrese su Email</label>
+        <input wire:model="email" class="border border-gray-400 rounded px-2 py-1 mb-4" type="text">
+        <label class="mb-1">Ingrese su Contraseña</label>
+        <input wire:model="password" class="border border-gray-400 rounded px-2 py-1 mb-4" type="password">
+        <div class="flex flex-col gap-2">
+            <button wire:click="ingresar" class="bg-green-600 text-white font-semibold py-2 rounded p-1">Ingresar</button>
+            {{-- <button wire:click="checkAuth" class="rounded p-1">Verificar</button> --}}
+            <label>{{ $reg }}</label>
+        </div>
+    </section>
 </div>
