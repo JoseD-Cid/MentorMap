e5cd149f7c3cb2a08403c58a278fef249b3401ba
TREBOR T <josedulanyrueda@gmail.com>
2025-12-22 11:20:28 -0600

    Eloquent

--- app/Livewire/Mentor/Dashboard.php
+++ app/Livewire/Mentor/Dashboard.php
@@ -4,12 +4,29 @@ namespace App\Livewire\Mentor;
 
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use App\Models\Discipline;
+use App\Models\Subject;
+use phpDocumentor\Reflection\Types\This;
 
 class Dashboard extends Component
 {
+    public $disciplines;
+    public $discipline_id = 2;
+    public $subjects;
+    public $subject_id;
+
     #[Layout('components.layouts.mentor')]
     public function render()
     {
         return view('livewire.mentor.dashboard');
+    }
+
+    public function mount() {
+        $this->disciplines = Discipline::all();
+        $this->subjects = Subject::where('discipline_id', $this->discipline_id)->get();
+    }
+
+    public function updatedDisciplineId($value) {
+        $this->subjects = Subject::where('discipline_id', $value)->get();
     }
 }
--- resources/views/livewire/mentor/dashboard.blade.php
+++ resources/views/livewire/mentor/dashboard.blade.php
@@ -1,18 +1,19 @@
 <div class="flex flex-row gap-4 p-4">
     <div class="flex flex-col">
         <label class="mb-2">Disciplinas</label>
-        <select class="border border-gray-400 rounded w-48">
-            <option value="">1</option>
-            <option value="">2</option>
-            <option value="">3</option>
+        <select wire:model.live="discipline_id" class="border border-gray-400 rounded w-48">
+            @foreach ($disciplines as $discipline)
+                <option value="{{ $discipline->id }}">{{ $discipline->name }}</option>
+            @endforeach
         </select>
     </div>
     <div class="flex flex-col">
         <label class="mb-2">Asignaturas</label>
-        <select class="border border-gray-400 rounded w-48">
-            <option value="">1</option>
-            <option value="">2</option>
-            <option value="">3</option>
+        <select wire:model="subject_id" class="border border-gray-400 rounded w-48">
+            @foreach ($subjects as $subject)
+
+                <option value="{{ $subject->id }}">{{ $subject->name }}</option>
+            @endforeach
         </select>
     </div>
 </div>
