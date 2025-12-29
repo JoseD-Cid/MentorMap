de74056a58ce9d4aff7b42590636c074fa88f267
Marioo83GitHub <mcarbajalg@unah.hn>
2025-12-28 20:02:21 -0600

    cambiado tamaño de iconos en navbar movil

--- resources/views/livewire/components/sidebar.blade.php
+++ resources/views/livewire/components/sidebar.blade.php
@@ -1,16 +1,16 @@
 <aside x-data="{ expanded: $persist(false) }" {{-- x-on:mousemove="expanded = true"
     x-on:mouseout="expanded = false" --}}
-    class="flex md:flex-col items-start justify-between text-white font-semibold md:h-screen bg-[#1A44D5] p-4 transition-all duration-200 ease-out relative z-0 overflow-hidden"
-    :class="expanded ? 'md:w-48 h-52' : 'md:w-16 h-18'">
+    class="flex md:flex-col items-start justify-between text-white font-semibold md:h-screen bg-[#1A44D5] p-4 transition-all duration-200 ease-in-out relative z-0 overflow-hidden"
+    :class="expanded ? 'md:w-48 h-34' : 'md:w-16 h-16'">
 
     {{-- Expand button --}}
     <div x-on:click="expanded = !expanded" class="flex gap-2 items-center active:scale-98">
-        <div class="flex max-w-8 md:h-12 h-10 shrink-0 active:text-red-400">
+        <div class="flex max-w-8 md:h-12 h-8 shrink-0 active:text-red-400">
             <x-mentor-map-logo phase="active white" />
         </div>
 
-        <p x-transition :class="expanded ? '' : 'hidden'"
-            class="text-lg md:text-xl active:text-gray-300 transition-all flex flex-col leading-none cursor-default select-none"
+        <p x-transition :class="expanded ? '' : 'md:hidden'"
+            class="text-base md:text-xl transition-all flex flex-col leading-none cursor-default select-none"
             style="font-family: 'Fredoka', sans-serif;">
             <span>Mentor</span>
             <span>Map</span>
@@ -18,34 +18,34 @@
     </div>
 
     {{-- Rest of the buttons --}}
-    <section class="flex flex-col gap-4" :class="expanded ? '' : 'hidden md:flex'">
+    <section class="flex flex-col gap-2 md:gap-4" :class="expanded ? '' : 'hidden md:flex'">
         {{-- Home --}}
         <a href="{{ route('mentor-dashboard') }}" wire:navigate
             class="flex-row-reverse md:flex-row hover:scale-103 transition-all rounded-lg active:scale-98 active:text-gray-300 flex gap-2 items-center whitespace-nowrap duration-100"
             :class="expanded ? 'md:justify-start' : ''">
-            <span class="icon-[solar--pie-chart-2-bold-duotone] size-8 shrink-0"></span>
-            <span class="text-sm" x-show="expanded" x-transition>Estadísticas</span>
+            <span class="icon-[solar--pie-chart-2-bold-duotone] size-5 md:size-8 shrink-0"></span>
+            <span class="text-xs md:text-sm" x-show="expanded" x-transition>Estadísticas</span>
         </a>
         {{-- Topics --}}
         <a href="{{ route('mentor-my-topics') }}" wire:navigate
             class="flex-row-reverse md:flex-row hover:scale-103 transition-all rounded-lg active:scale-98 active:text-gray-300 flex gap-2 items-center whitespace-nowrap duration-100"
             :class="expanded ? 'md:justify-start' : ''">
-            <span class="icon-[solar--book-bookmark-bold-duotone] size-8 shrink-0"></span>
-            <span class="text-sm" x-show="expanded" x-transition>Temario</span>
+            <span class="icon-[solar--book-bookmark-bold-duotone] size-5 md:size-8 shrink-0"></span>
+            <span class="text-xs md:text-sm" x-show="expanded" x-transition>Temario</span>
         </a>
         {{-- Location --}}
         <a href="{{ route('mentor-my-location') }}" wire:navigate
             class="flex-row-reverse md:flex-row hover:scale-103 transition-all rounded-lg active:scale-98 active:text-gray-300 flex gap-2 items-center whitespace-nowrap duration-100"
             :class="expanded ? 'md:justify-start' : ''">
-            <span class="icon-[solar--map-point-wave-bold-duotone] size-8 shrink-0"></span>
-            <span class="text-sm" x-show="expanded" x-transition>Ubicación</span>
+            <span class="icon-[solar--map-point-wave-bold-duotone] size-5 md:size-8 shrink-0"></span>
+            <span class="text-xs md:text-sm" x-show="expanded" x-transition>Ubicación</span>
         </a>
         {{-- Logout --}}
         <button
             class="hover:text-red-300 flex-row-reverse md:flex-row hover:scale-103 transition-all rounded-lg active:scale-98 active:text-gray-300 flex gap-2 items-center whitespace-nowrap duration-100"
             :class="expanded ? 'md:justify-start' : ''">
-            <span class="icon-[solar--exit-bold-duotone] size-8 shrink-0"></span>
-            <span class="text-sm" x-show="expanded" x-transition>Cerrar Sesión</span>
+            <span class="icon-[solar--exit-bold-duotone] size-5 md:size-8 shrink-0"></span>
+            <span class="text-xs md:text-sm" x-show="expanded" x-transition>Cerrar Sesión</span>
         </button>
     </section>
 </aside>
--- resources/views/livewire/mentor/dashboard.blade.php
+++ resources/views/livewire/mentor/dashboard.blade.php
@@ -1,3 +1,13 @@
-<div class="p-4 h-full">
-    <span>Estadísticas</span>
+<div class="flex flex-col overflow-auto h-full">
+    <header class="text-xl font-semibold h-max p-4 shadow-amber-400">
+        <h1>Estadísticas</h1>
+    </header>
+
+    <main class="bg-gray-100 flex flex-col h-full overflow-auto p-4">
+        <span>Cards</span>
+    </main>
+
+    <footer>
+
+    </footer>
 </div>
