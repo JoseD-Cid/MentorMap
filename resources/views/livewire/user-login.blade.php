2659a79536914dbec89caece53b52d0fa72f41b0
TREBOR T <josedulanyrueda@gmail.com>
2025-12-29 16:41:06 -0600

    Registro de Usuarios en Login

--- resources/views/livewire/user-login.blade.php
+++ resources/views/livewire/user-login.blade.php
@@ -1,17 +1,43 @@
-<div class="flex h-screen items-center justify-center">
+<div x-data="{ login: true }" class="flex h-screen items-center justify-center" x-transition>
+    <div class="flex flex-col w-96 p-6 rounded-lg shadow-md shadow-gray-300 border border-gray-300">
+        <div class="relative h-72">
+            {{-- Relative va de la mano con  la clase absolute, para posicionar elementos especificos en la ubicación deseada --}}
+            <section x-transition x-show="login"
+                class="flex absolute inset-0 flex-col h-72">
+                <h2 class="text-center mb-4 font-semibold text-lg">Iniciar Sesión</h2>
 
-    <section class="flex flex-col w-72 p-6 rounded-lg shadow-md shadow-gray-300 border border-gray-300">
+                <label class="mb-1">Ingrese su Email</label>
+                <input wire:model="email" class="border border-gray-400 rounded px-2 py-1 mb-4" type="text">
+                <label class="mb-1">Ingrese su Contraseña</label>
+                <input wire:model="password" class="border border-gray-400 rounded px-2 py-1 mb-4" type="password">
+                <div class="flex flex-col gap-2">
+                    <button wire:click="ingresar"
+                        class="bg-green-600 text-white font-semibold py-2 rounded p-1">Ingresar</button>
+                    <label>{{ $reg }}</label>
+                </div>
+                <div>
+                    <button x-on:click="login = ! login"
+                        class="text-blue-600 font-semibold py-2 rounded p-1 underline">Registrarse</button>
+                </div>
+            </section>
 
-        <h2 class="text-center mb-4 font-semibold text-lg">Iniciar Sesión</h2>
-
-        <label class="mb-1">Ingrese su Email</label>
-        <input wire:model="email" class="border border-gray-400 rounded px-2 py-1 mb-4" type="text">
-        <label class="mb-1">Ingrese su Contraseña</label>
-        <input wire:model="password" class="border border-gray-400 rounded px-2 py-1 mb-4" type="password">
-        <div class="flex flex-col gap-2">
-            <button wire:click="ingresar" class="bg-green-600 text-white font-semibold py-2 rounded p-1">Ingresar</button>
-            {{-- <button wire:click="checkAuth" class="rounded p-1">Verificar</button> --}}
-            <label>{{ $reg }}</label>
+            <section x-transition x-show="!login"
+                class="flex absolute inset-0  flex-col h-72">
+                <h2 class="text-center mb-4 font-semibold text-lg">Registrarse</h2>
+                <label class="mb-1">Ingrese su Email</label>
+                <input wire:model="email" class="border border-gray-400 rounded px-2 py-1 mb-4" type="text">
+                <label class="mb-1">Ingrese su Contraseña</label>
+                <input wire:model="password" class="border border-gray-400 rounded px-2 py-1 mb-4" type="password">
+                <div class="flex flex-col gap-2">
+                    <button wire:click="ingresar"
+                        class="bg-green-60[0 text-white font-semibold py-2 rounded p-1">Ingresar</button>
+                    <label>{{ $reg }}</label>
+                </div>
+                <div>
+                    <button x-on:click="login = ! login"
+                        class="text-blue-600 font-semibold py-2 rounded p-1 underline">Iniciar Sesión</button>
+                </div>
+            </section>
         </div>
-    </section>
+    </div>
 </div>
