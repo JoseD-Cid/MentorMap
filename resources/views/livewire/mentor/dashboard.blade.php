5f440c0dab635624235b952f3da59fe4b57e37ab
Marioo83GitHub <mcarbajalg@unah.hn>
2025-12-30 19:49:36 -0600

    machote inicial de dashboard y cambio de fuente en layouts a Nunito

--- resources/views/livewire/mentor/dashboard.blade.php
+++ resources/views/livewire/mentor/dashboard.blade.php
@@ -1,13 +1,33 @@
-<div class="flex flex-col overflow-auto h-full">
-    <header class="text-xl font-semibold h-max p-4 shadow-amber-400">
-        <h1>Estadísticas</h1>
+<div class="flex flex-col gap-4 p-4 min-h-screen overflow-auto bg-slate-100">
+    <header class="text-xl font-bold p-4 bg-white rounded-xl shadow">
+        <h1>Dashboard</h1>
     </header>
 
-    <main class="bg-gray-100 flex flex-col h-full overflow-auto p-4">
-        <span>Cards</span>
+    <main class="flex flex-col flex-1">
+        <div class="flex md:flex-row flex-col gap-4">
+            <article class="flex rounded-xl w-56 items-center bg-white shadow p-4">
+                <span class="icon-[lets-icons--date-today-duotone] size-12 text-mmblue"></span>
+                <div class="flex flex-col ml-2">
+                    <span class="text-sm text-gray-500 font-semibold">Sesiones Finalizadas</span>
+                    <span class="text-2xl font-bold">32</span>
+                </div>
+            </article>
+
+            <article class="flex rounded-xl w-56 items-center bg-white shadow p-4">
+                <span class="icon-[solar--stars-bold-duotone] size-12 text-amber-400"></span>
+                <div class="flex flex-col ml-2">
+                    <span class="text-sm text-gray-500 font-semibold">Rating Promedio</span>
+                    <span class="text-2xl font-bold">4.72</span>
+                </div>
+            </article>
+
+            <article class="flex rounded-xl w-max items-center bg-white shadow p-4">
+                <span class="icon-[solar--wallet-bold-duotone] size-12 text-mmgreen"></span>
+                <div class="flex flex-col ml-2">
+                    <span class="text-sm text-gray-500 font-semibold">Billetera</span>
+                    <span class="text-2xl font-bold">L 97,000.32</span>
+                </div>
+            </article>
+        </div>
     </main>
-
-    <footer>
-
-    </footer>
 </div>
