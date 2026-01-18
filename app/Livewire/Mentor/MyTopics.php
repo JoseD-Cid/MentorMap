c40130d15bfe8d5cc238761dd544587741a67cb1
TREBOR T <josedulanyrueda@gmail.com>
2026-01-18 08:07:20 -0600

    CRUD y instalación Daisy UI

--- app/Livewire/Mentor/MyTopics.php
+++ app/Livewire/Mentor/MyTopics.php
@@ -8,22 +8,24 @@ use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Topic;
 use App\Models\Subject;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Livewire\WithPagination;
 
 class MyTopics extends Component
 {
-    //Modal control
+    use WithPagination;
+
+    // Modal control
     public $showModal = false;
 
     // Variables for topic creation
     public $discipline_id;
     public $subject_id;
-    public $subjects;
+    public $subjects = [];
     public $topic_name;
     public $disciplines;
     public $selectedDiscipline = null;
     public $query = '';
-    public $topics;
+    public $topicIdEdited;
 
     public function openModal()
     {
@@ -32,38 +34,37 @@ class MyTopics extends Component
 
     public function closeModal()
     {
+        $this->reset(['topicIdEdited', 'topic_name', 'subject_id', 'selectedDiscipline', 'subjects']);
         $this->showModal = false;
     }
-
 
     #[Layout('layouts.mentor')]
     public function render()
     {
-        return view('livewire.mentor.my-topics');
+        $mentorId = Auth::user()->mentor->id;
+
+        $topics = Topic::with(['subject.discipline'])
+            ->where('mentor_id', $mentorId)
+            ->where('name', 'like', '%' . $this->query . '%')
+            ->paginate(5);
+
+        return view('livewire.mentor.my-topics', [
+            'topics' => $topics
+        ]);
     }
 
     public function mount()
     {
         $this->disciplines = Discipline::all();
-        $this->subjects = [];
-
-        $mentorId  = Auth::user()->mentor->id;
-
-        $this->topics = Topic::with(['subject.discipline'])
-            ->where('mentor_id', $mentorId)
-            ->get();
     }
 
     public function updatedSelectedDiscipline($disciplineId)
     {
         $this->discipline_id = $disciplineId;
 
-        if(!empty($disciplineId))
-        {
+        if (!empty($disciplineId)) {
             $this->subjects = Subject::where('discipline_id', $disciplineId)->get();
-        }
-        else
-        {
+        } else {
             $this->subjects = [];
         }
     }
@@ -72,41 +73,67 @@ class MyTopics extends Component
     {
         $this->validate([
             'topic_name' => 'required|string|max:155',
+            'subject_id' => 'required|exists:subjects,id',
         ]);
 
-        $mentorId  = Auth::user()->mentor->id;
+        $mentorId = Auth::user()->mentor->id;
         Topic::create([
             'mentor_id' => $mentorId,
             'subject_id' => $this->subject_id,
             'name' => $this->topic_name,
         ]);
 
+        session()->flash('message', 'Tema creado exitosamente.');
         $this->closeModal();
-
     }
-
-
-
 
     public function updatedQuery()
     {
-        $mentorId  = Auth::user()->mentor->id;
-        $this->topics = Topic::with(['subject.discipline'])
-            ->where('mentor_id', $mentorId)
-            ->where('name', 'like', '%' . $this->query . '%')
-            ->get();
+        $this->resetPage();
     }
 
-    public function deleteTopic()
+    public function deleteTopic($id)
     {
+        $topic = Topic::findOrFail($id);
+        $topic->delete();
 
+        session()->flash('message', 'Tema eliminado exitosamente.');
     }
 
-    public function editTopic()
+    public function editTopic($id)
     {
+        $this->topicIdEdited = $id;
+        $topic = Topic::findOrFail($id);
+        $this->topic_name = $topic->name;
+        $this->subject_id = $topic->subject->id;
+        $this->selectedDiscipline = $topic->subject->discipline->id;
+        $this->subjects = Subject::where('discipline_id', $this->selectedDiscipline)->get();
 
+        $this->openModal();
     }
 
+    public function updateTopic()
+    {
+        $this->validate([
+            'topic_name' => 'required|string|max:155',
+            'subject_id' => 'required|exists:subjects,id',
+        ]);
 
+        $topic = Topic::findOrFail($this->topicIdEdited);
+        $topic->name = $this->topic_name;
+        $topic->subject_id = $this->subject_id;
+        $topic->save();
 
+        session()->flash('message', 'Tema actualizado exitosamente.');
+    }
+
+    public function save()
+    {
+        if ($this->topicIdEdited) {
+            $this->updateTopic();
+        } else {
+            $this->createTopic();
+        }
+        $this->closeModal();
+    }
 }
---
+++ lang/en/pagination.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Siguiente &raquo;',
+
+    // This is the one you want to change:
+    'showing' => 'Mostrando :first a :last de :total resultados',
+
+    // Or more variations:
+    'results' => 'Mostrando :first-:last de :total',
+
+];
